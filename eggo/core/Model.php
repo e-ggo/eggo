c736d263e8bab734a7e2025227214479bc05112d
eggo <100505355+e-ggo@users.noreply.github.com>
2022-02-27 17:17:08 +0800

    Update Model.php

--- eggo/core/Model.php
+++ eggo/core/Model.php
@@ -2,29 +2,11 @@
 declare(strict_types=1);
 namespace Eggo\core;
 
-use fastphp\db\Sql;
-
-class Model extends Sql
+class Model extends db
 {
     protected $model;
     /**
      * @var string
      */
     private $table;
-
-    public function __construct()
-    {
-        // 获取数据库表名
-        if (!$this->table) {
-
-            // 获取模型类名称
-            $this->model = get_class($this);
-
-            // 删除类名最后的 Model 字符
-            $this->model = substr($this->model, 0, -5);
-
-            // 数据库表名与类名一致
-            $this->table = strtolower($this->model);
-        }
-    }
 }
